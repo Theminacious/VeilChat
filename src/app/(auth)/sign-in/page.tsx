f09242387fb91182b06a39ba5138606df43bd9d8
theminacious <zacksunrise32@gmail.com>
2024-06-12 15:53:54 +0530

    "Updated sign-in and sign-up pages with new design and animations using Framer Motion, and refactored code to improve readability and maintainability."

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,177 +1,143 @@
-'use client'
-import React, { useEffect, useState } from 'react'
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z  from "zod"
-import Link from 'next/link'
-import {useDebounceValue} from 'usehooks-ts'
-import { useToast } from "@/components/ui/use-toast"
-import { useRouter } from 'next/navigation'
-import { signUpSchema } from '@/schemas/signUpSchema'
-import axios, { AxiosError } from 'axios'
-import ApiResponse from '@/types/ApiResponse'
-import { Form, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
-import { Loader2 } from 'lucide-react'
+'use client';
 
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { signIn } from 'next-auth/react';
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useToast } from '@/components/ui/use-toast';
+import { signInSchema } from '@/schemas/signInSchema';
+import { motion } from 'framer-motion';
 
+export default function SignInForm() {
+  const router = useRouter();
 
-const page = () => {
-  const [username,setUsername] = useState('')
-  const [usernameMessage,setUsernameMessage] = useState('')
-  const [isCheckingUsername,setIsCheckingUsername] = useState(false)
-  const [isSubmitting,setIsSubmitting] = useState(false)
-  const debouncedUsername=useDebounceValue(username,300)
-  const { toast } = useToast()
-  const router = useRouter()
-
-  // zod implementation
-  const form  = useForm<z.infer<typeof signUpSchema>>({
-    resolver: zodResolver(signUpSchema),
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
     defaultValues: {
-      username: "",
-      email: "",
-      password: "",
+      identifier: '',
+      password: '',
     },
-  })
+  });
 
-  useEffect(() => {
-   const checkUsernameUnique = async()=>{
-    if( debouncedUsername){
-      setIsCheckingUsername(true)
-      setUsernameMessage('')
-      try {
-        const response = await axios.get(`/api/check-username-unique?username=${debouncedUsername}`)
-        setUsernameMessage(response.data.message)
-      } catch (error) {
-        const axiosError = error as AxiosError<ApiResponse>;
-        setUsernameMessage(
-          axiosError.response?.data.message ?? "Error checking username"
-        )
-        
-      }finally{
-        setIsCheckingUsername(false)
+  const { toast } = useToast();
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    const result = await signIn('credentials', {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password,
+    });
+
+    if (result?.error) {
+      if (result.error === 'CredentialsSignin') {
+        toast({
+          title: 'Login Failed',
+          description: 'Incorrect username or password',
+          variant: 'destructive',
+        });
+      } else {
+        toast({
+          title: 'Error',
+          description: result.error,
+          variant: 'destructive',
+        });
       }
-   }
-    
     }
 
-    checkUsernameUnique()
-  }, [debouncedUsername])
-
-   const onSubmit = async(data:z.infer<typeof signUpSchema>)=>{
-    setIsSubmitting(true)
-    try {
-      const response = await axios.post<ApiResponse>('/api/sign-up',data)
-      toast({
-        title: "Account created",
-        description: response.data.message,
-      })
-      router.replace(`/verify/${username}`)
-      setIsSubmitting(false)
-    } catch (error) {
-      console.error("Error in signup of user")
-      const axiosError = error as AxiosError<ApiResponse>;
-      toast({
-        title: "Signup failed",
-        description: axiosError.response?.data.message ?? "Something went wrong",
-        variant: "destructive",
-      })
-      setIsSubmitting(false)
+    if (result?.url) {
+      router.replace('/dashboard');
     }
-   }
-  
-
-
+  };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-800">
-    <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
-      <div className="text-center">
-        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-          Join VeilChat
-        </h1>
-        <p className="mb-4">Sign up to start your anonymous adventure</p>
-      </div>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-          <FormField
-            name="username"
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <Input  placeholder='Enter Username'
-                  {...field}
-                  onChange={(e) => {
-                    field.onChange(e);
-                    setUsername(e.target.value);
-                  }}
-                />
-                {isCheckingUsername && <Loader2 className="animate-spin" />}
-                {!isCheckingUsername && usernameMessage && (
-                  <p
-                    className={`text-sm ${
-                      usernameMessage === 'Username is unique'
-                        ? 'text-green-500'
-                        : 'text-red-500'
-                    }`}
-                  >
-                    {usernameMessage}
-                  </p>
-                )}
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            name="email"
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <Input {...field} name="email" />
-                <p className='text-muted text-gray-400 text-sm'>We will send you a verification code</p>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            name="password"
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <Input type="password" {...field} name="password" />
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit" className='w-full' disabled={isSubmitting}>
-            {isSubmitting ? (
-              <>
-                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                Please wait
-              </>
-            ) : (
-              'Sign Up'
-            )}
-          </Button>
-        </form>
-      </Form>
-      <div className="text-center mt-4">
-        <p>
-          Already a member?{' '}
-          <Link href="/sign-in" className="text-blue-600 hover:text-blue-800">
-            Sign in
-          </Link>
-        </p>
-      </div>
+    <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-gray-900 via-black to-gray-900">
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className="w-full max-w-md p-8 space-y-8 bg-gray-800 rounded-lg shadow-lg"
+      >
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          className="text-center"
+        >
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6 text-white">
+            Welcome Back to VeilChat
+          </h1>
+          <p className="mb-4 text-gray-400">Sign in to continue your secret conversations</p>
+        </motion.div>
+        <Form {...form}>
+          <motion.form
+            initial={{ scale: 0.9 }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 0.5 }}
+            whileHover={{ scale: 1.02 }}
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-6"
+          >
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-gray-300">Email/Username</FormLabel>
+                  <Input 
+                    {...field} 
+                    className="w-full border-gray-600 bg-gray-700 text-white rounded-md shadow-sm focus:ring focus:ring-blue-500"
+                  />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-gray-300">Password</FormLabel>
+                  <Input 
+                    type="password" 
+                    {...field} 
+                    className="w-full border-gray-600 bg-gray-700 text-white rounded-md shadow-sm focus:ring focus:ring-blue-500"
+                  />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button 
+              className='w-full bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 focus:ring focus:ring-blue-300' 
+              type="submit"
+            >
+              Sign In
+            </Button>
+          </motion.form>
+        </Form>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          className="text-center mt-4"
+        >
+          <p className="text-gray-400">
+            Not a member yet?{' '}
+            <Link href="/sign-up" className="text-blue-500 hover:text-blue-400">
+              Sign up
+            </Link>
+          </p>
+        </motion.div>
+      </motion.div>
     </div>
-  </div>
-  )
+  );
 }
-
-export default page
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -2,9 +2,9 @@
 import React, { useEffect, useState } from 'react'
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-import * as z  from "zod"
+import * as z from "zod"
 import Link from 'next/link'
-import {useDebounceCallback, useDebounceValue} from 'usehooks-ts'
+import { useDebounceCallback } from 'usehooks-ts'
 import { useToast } from "@/components/ui/use-toast"
 import { useRouter } from 'next/navigation'
 import { signUpSchema } from '@/schemas/signUpSchema'
@@ -14,20 +14,18 @@ import { Form, FormField, FormItem, FormLabel, FormMessage } from '@/components/
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { Loader2 } from 'lucide-react'
+import { motion } from 'framer-motion'
 
-
-
-const page = () => {
-  const [username,setUsername] = useState('')
-  const [usernameMessage,setUsernameMessage] = useState('')
-  const [isCheckingUsername,setIsCheckingUsername] = useState(false)
-  const [isSubmitting,setIsSubmitting] = useState(false)
-  const debounced=useDebounceCallback(setUsername,500)
+const SignUp = () => {
+  const [username, setUsername] = useState('')
+  const [usernameMessage, setUsernameMessage] = useState('')
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const debounced = useDebounceCallback(setUsername, 500)
   const { toast } = useToast()
   const router = useRouter()
 
-  // zod implementation
-  const form  = useForm<z.infer<typeof signUpSchema>>({
+  const form = useForm<z.infer<typeof signUpSchema>>({
     resolver: zodResolver(signUpSchema),
     defaultValues: {
       username: "",
@@ -37,33 +35,31 @@ const page = () => {
   })
 
   useEffect(() => {
-   const checkUsernameUnique = async()=>{
-    if( username){
-      setIsCheckingUsername(true)
-      setUsernameMessage('')
-      try {
-        const response = await axios.get(`/api/check-username-unique?username=${username}`)
-        setUsernameMessage(response.data.message)
-      } catch (error) {
-        const axiosError = error as AxiosError<ApiResponse>;
-        setUsernameMessage(
-          axiosError.response?.data.message ?? "Error checking username"
-        )
-        
-      }finally{
-        setIsCheckingUsername(false)
+    const checkUsernameUnique = async () => {
+      if (username) {
+        setIsCheckingUsername(true)
+        setUsernameMessage('')
+        try {
+          const response = await axios.get(`/api/check-username-unique?username=${username}`)
+          setUsernameMessage(response.data.message)
+        } catch (error) {
+          const axiosError = error as AxiosError<ApiResponse>
+          setUsernameMessage(
+            axiosError.response?.data.message ?? "Error checking username"
+          )
+        } finally {
+          setIsCheckingUsername(false)
+        }
       }
-   }
-    
     }
 
     checkUsernameUnique()
   }, [username])
 
-   const onSubmit = async(data:z.infer<typeof signUpSchema>)=>{
+  const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
     setIsSubmitting(true)
     try {
-      const response = await axios.post<ApiResponse>('/api/sign-up',data)
+      const response = await axios.post<ApiResponse>('/api/sign-up', data)
       toast({
         title: "Account created",
         description: response.data.message,
@@ -72,7 +68,7 @@ const page = () => {
       setIsSubmitting(false)
     } catch (error) {
       console.error("Error in signup of user")
-      const axiosError = error as AxiosError<ApiResponse>;
+      const axiosError = error as AxiosError<ApiResponse>
       toast({
         title: "Signup failed",
         description: axiosError.response?.data.message ?? "Something went wrong",
@@ -80,98 +76,118 @@ const page = () => {
       })
       setIsSubmitting(false)
     }
-   }
-  
-
-
+  }
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-800">
-    <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
-      <div className="text-center">
-        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-          Join VeilChat
-        </h1>
-        <p className="mb-4">Sign up to start your anonymous adventure</p>
-      </div>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-          <FormField
-            name="username"
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <Input  placeholder='Enter Username'
-                  {...field}
-                  onChange={(e) => {
-                    field.onChange(e);
-                    debounced(e.target.value);
-                  }}
-                />
-                {isCheckingUsername && <Loader2 className="animate-spin" />}
-                {!isCheckingUsername && usernameMessage && (
-                  <p
-                    className={`text-sm ${
-                      usernameMessage === 'Username is unique'
+    <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-gray-900 via-black to-gray-900">
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className="w-full max-w-md p-8 space-y-8 bg-gray-800 rounded-lg shadow-lg"
+      >
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          className="text-center"
+        >
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6 text-white">
+            Join VeilChat
+          </h1>
+          <p className="mb-4 text-gray-400">Sign up to start your anonymous adventure</p>
+        </motion.div>
+        <Form {...form}>
+          <motion.form
+            initial={{ scale: 0.9 }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 0.5 }}
+            whileHover={{ scale: 1.02 }}
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-6"
+          >
+            <FormField
+              name="username"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-gray-300">Username</FormLabel>
+                  <Input
+                    placeholder='Enter Username'
+                    {...field}
+                    className="w-full border-gray-600 bg-gray-700 text-white rounded-md shadow-sm focus:ring focus:ring-blue-500"
+                    onChange={(e) => {
+                      field.onChange(e)
+                      debounced(e.target.value)
+                    }}
+                  />
+                  {isCheckingUsername && <Loader2 className="animate-spin text-white" />}
+                  {!isCheckingUsername && usernameMessage && (
+                    <p
+                      className={`text-sm ${usernameMessage === 'Username is unique'
                         ? 'text-green-500'
                         : 'text-red-500'
-                    }`}
-                  >
-                    {usernameMessage}
-                  </p>
-                )}
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            name="email"
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <Input {...field} name="email" />
-                <p className='text-muted text-gray-400 text-sm'>We will send you a verification code</p>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+                        }`}
+                    >
+                      {usernameMessage}
+                    </p>
+                  )}
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="email"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-gray-300">Email</FormLabel>
+                  <Input {...field} className="w-full border-gray-600 bg-gray-700 text-white rounded-md shadow-sm focus:ring focus:ring-blue-500" />
+                  <p className='text-gray-400 text-sm'>We will send you a verification code</p>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <FormField
-            name="password"
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <Input type="password" {...field} name="password" />
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit" className='w-full' disabled={isSubmitting}>
-            {isSubmitting ? (
-              <>
-                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                Please wait
-              </>
-            ) : (
-              'Sign Up'
-            )}
-          </Button>
-        </form>
-      </Form>
-      <div className="text-center mt-4">
-        <p>
-          Already a member?{' '}
-          <Link href="/sign-in" className="text-blue-600 hover:text-blue-800">
-            Sign in
-          </Link>
-        </p>
-      </div>
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-gray-300">Password</FormLabel>
+                  <Input type="password" {...field} className="w-full border-gray-600 bg-gray-700 text-white rounded-md shadow-sm focus:ring focus:ring-blue-500" />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className='w-full bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 focus:ring focus:ring-blue-300' disabled={isSubmitting}>
+              {isSubmitting ? (
+                <>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin text-white" />
+                  Please wait
+                </>
+              ) : (
+                'Sign Up'
+              )}
+            </Button>
+          </motion.form>
+        </Form>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          className="text-center mt-4"
+        >
+          <p className="text-gray-400">
+            Already a member?{' '}
+            <Link href="/sign-in" className="text-blue-500 hover:text-blue-400">
+              Sign in
+            </Link>
+          </p>
+        </motion.div>
+      </motion.div>
     </div>
-  </div>
   )
 }
 
-export default page
+export default SignUp
